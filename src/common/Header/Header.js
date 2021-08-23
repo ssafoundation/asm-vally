4b61b6c8225889ba46599959897c47527ed29531
Akhtaruzzaman <akhtr01947@gmail.com>
2021-08-23 23:49:48 +0600

    update

--- src/common/Header/Header.js
+++ src/common/Header/Header.js
@@ -1,7 +1,7 @@
 import React from "react";
-import Header_Bottom from "./Header_Bottom";
-import Header_Middle from "./Header_Middle";
-import Header_Top from "./Header_Top";
+import HeaderBottom from "./HeaderBottom";
+import HeaderMiddle from "./HeaderMiddle";
+import HeaderTop from "./HeaderTop";
 import MainCart from "./Other/MainCart";
 import MainNavbar from "./Other/MainNavbar";
 import ProductAddCart from "./Other/ProductAddCart";
@@ -15,9 +15,9 @@ const Header = () => {
       <header className="header-main-area">
         <div className="container-fluid g-0">
           <div className="header-padding-area">
-            <Header_Top />
-            <Header_Middle />
-            <Header_Bottom />
+            <HeaderTop />
+            <HeaderMiddle />
+            <HeaderBottom />
           </div>
         </div>
       </header>
